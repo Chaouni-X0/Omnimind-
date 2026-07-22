affca59a64fb38913b57126432f5a8ccd514a398
Chaouni-X0 <2.79552036e+08+Chaouni-X0@users.noreply.github.com>
2026-07-22 15:06:32 +0000

    Fix: Increase minSdk to 26 to support java.lang.Process#waitFor and pass Lint checks

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -65,7 +65,7 @@ const config: ExpoConfig = {
       {
         android: {
           buildArchs: ["armeabi-v7a", "arm64-v8a"],
-          minSdkVersion: 24,
+          minSdkVersion: 26,
         },
       },
     ],
